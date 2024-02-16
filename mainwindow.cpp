b6c4abac7ffa6b7d56688b1a306624eef3f396fe
admHipepr <157802044+admHipepr@users.noreply.github.com>
2024-02-16 11:57:58 +0100

    Add files via upload

---
+++ mainwindow.cpp
@@ -0,0 +1,51 @@
+
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    int n = elementy.length();
+    for(int i=0; i<n; i++)
+        for(int j=1; j<n - 1; j++)
+            if(elementy[j=1]>elementy[j]){
+                int temp = elementy[j-1];
+                elementy[j-1] = elementy[j];
+                elementy[j] = temp;
+            }
+    QString elementyStr = "";
+    QString el;
+    for(int  i=0; i<n; i++){
+        el = QString :: number(elementy[i]);
+        elementyStr +=el + "";
+        QMessageBox msg(this);
+        msg.setText(elementyStr);
+        msg.setStandardButtons(QMessageBox::Ok);
+        msg.setWindowTitle("Posortowane liczby");
+        msg.exec();
+    }
+}
+
+
+
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    elementy.append(ui->spinBox->value());
+}
+
